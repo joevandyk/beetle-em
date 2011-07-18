8fbea165c1cfc7c92104c452fdb9455f18c1ff7d
Joe Van Dyk <joe@fixieconsulting.com>
2011-07-17 21:53:44 -0700

    cleanup

--- server.rb
+++ server.rb
@@ -9,13 +9,8 @@ client.register_queue(:search)
 
 client.register_handler(:search) do |m|
   query = m.data
-
-  result = []
   doc = Nokogiri::HTML(open("http://www.google.com/search?q=#{URI.encode(query)}").read)
-  doc.css('h3.r a.l').each do |link|
-    result << link.content
-  end
-  result.to_json
+  doc.css('h3.r a.l').map { |link| link.content }.to_json
 end
 
 client.listen do
